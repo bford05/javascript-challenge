2a93eb0c0a441898d9fdb5c5a021f23b1de57105
Brian <cbd.belapur.15@gmail.com>
2020-05-09 16:09:39 -0400

    update form filter code and add error message

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,8 +1,11 @@
 // define variable for table body 
 var tbody = d3.select("tbody");
 
+// Define variable for data in js file
+var ufo = data;
+
 // Create loop for ufo sighting details
-data.forEach((ufoDetails) => {
+ufo.forEach((ufoDetails) => {
     console.log(ufoDetails);
     // Append table row for each UFO sighting
     var row = tbody.append("tr");
@@ -14,9 +17,6 @@ data.forEach((ufoDetails) => {
         cell.text(value);
     });
 });
-
-// Define variable for data in js file
-var ufo = data;
 
 // Select button on form for date filtering
 var button = d3.select("#filter-btn");
@@ -31,6 +31,8 @@ form.on("submit", runEnter);
 
 // Create funcion to run for both clicking the button and pressing enter
 function runEnter() {
+    // Clear out table on html prior 
+    tbody.html("")
     // Prevent the page from refreshing
     d3.event.preventDefault();
     // Define variable for input date and select id that stores the date
@@ -38,10 +40,38 @@ function runEnter() {
     // Define variable for value property of input date
     var inputValue = inputDate.property("value");
     console.log(inputValue)
-    
-    var filteredData = ufo.filter(info => info.datetime === inputValue);
-    console.log(filteredData);
 
-    if (inputValue === datetime)
-    d3.select("input").text(inputValue);
-};
+    // Create empty list for search parameter date
+    inputDateMatch = []
+
+    // Use the form input to filter the data by date
+    ufo.forEach(event => {
+        Object.values(event).forEach(value =>{
+            if (inputValue === value ){
+                inputDateMatch.push(event);      
+                    }
+                }
+            );
+        }
+    );
+    console.log(inputDateMatch);
+
+    //Append table to show data matching search
+    inputDateMatch.forEach((dateMatch) => {
+        var row = tbody.append("tr");
+        Object.entries(dateMatch).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+        });
+      });
+
+    //Message to display if there are no matching results
+    if (inputDateMatch.length == 0) {
+        console.log("There is no data matching your search.")
+        d3.select("tbody")
+        .append("tr")
+        .append("td")
+          .attr("colspan", 7)
+          .html("<h4>There is no data matching your search.</h4>");
+    }
+}
